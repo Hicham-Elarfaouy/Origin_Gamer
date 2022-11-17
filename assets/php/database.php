fcb9021b1e72a6160a7c024e3e4a45a832b081d0
Hicham El Arfaouy <hicham.elarfaoui.1@gmail.com>
2022-11-17 10:56:44 +0100

    implement environment variables with package vlucas dotenv

--- assets/php/database.php
+++ assets/php/database.php
@@ -1,7 +1,11 @@
 <?php
+require '../../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
 function connection(){
-    $link = mysqli_connect('', '', '', '');
+    $link = mysqli_connect($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD'], $_ENV['DB_NAME']);
 
     if(!$link){
         die("ERROR: Could not connect. " . mysqli_connect_error());
