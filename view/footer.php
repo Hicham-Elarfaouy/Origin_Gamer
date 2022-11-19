5d6e9327b65c0aa6ca7985f3c0b95c86849efe7a
Hicham El Arfaouy <hicham.elarfaoui.1@gmail.com>
2022-11-19 14:37:55 +0100

    Add footer & list products front end

--- view/footer.php
+++ view/footer.php
@@ -1,3 +1,75 @@
+<footer class="bg-secondary container-fluid mt-5">
+    <div class="row p-5">
+        <div class="col-md-3 col-sm-6 text-white">
+            <h5>
+                <a class="nav-link mb-3" href="../index.php">Origin Gamer</a>
+            </h5>
+            <p>
+                <a class="nav-link" href="../index.php">Home</a>
+            </p>
+            <p>
+                <a class="nav-link" href="../index.php">Pc Components</a>
+            </p>
+            <p>
+                <a class="nav-link" href="../index.php">Gaming Pcs</a>
+            </p>
+            <p>
+                <a class="nav-link" href="../index.php">Contact Us</a>
+            </p>
+        </div>
+        <div class="col-md-3 col-sm-6 text-white">
+            <h5>
+                <a class="nav-link mb-3" href="../index.php">Pc Components</a>
+            </h5>
+            <p>
+                <a class="nav-link" href="../index.php">CPUs</a>
+            </p>
+            <p>
+                <a class="nav-link" href="../index.php">MotherBoards</a>
+            </p>
+            <p>
+                <a class="nav-link" href="../index.php">Memory</a>
+            </p>
+            <p>
+                <a class="nav-link" href="../index.php">Storage</a>
+            </p>
+            <p>
+                <a class="nav-link" href="../index.php">Peripherals</a>
+            </p>
+        </div>
+        <div class="col-md-3 col-sm-6 text-white">
+            <h5>
+                <a class="nav-link mb-3" href="../index.php">Gaming Pcs</a>
+            </h5>
+            <p>
+                <a class="nav-link" href="../index.php">Gaming Desktops</a>
+            </p>
+            <p>
+                <a class="nav-link" href="../index.php">Gaming Laptops</a>
+            </p>
+            <p>
+                <a class="nav-link" href="../index.php">PreBuild Pcs</a>
+            </p>
+        </div>
+        <div class="col-md-3 col-sm-6 text-white">
+            <h5>
+                <a class="nav-link mb-3" href="../index.php">Get in touch</a>
+            </h5>
+            <p>
+                <a class="nav-link" href="../index.php"><img class="me-2" src="../assets/img/linkedin.png" style="width: 30px">Linkedin</a>
+            </p>
+            <p>
+                <a class="nav-link" href="../index.php"><img class="me-2" src="../assets/img/github.png" style="width: 30px">Github</a>
+            </p>
+            <p>
+                <a class="nav-link" href="../index.php"><img class="me-2" src="../assets/img/twitter.png" style="width: 30px">Twitter</a>
+            </p>
+            <p>
+                <a class="nav-link" href="../index.php"><img class="me-2" src="../assets/img/facebook.png" style="width: 30px">Facebook</a>
+            </p>
+        </div>
+    </div>
+</footer>
 <!-- BEGIN JS -->
 <script src="../assets/js/vendor.min.js"></script>
 <script src="../assets/js/app.min.js"></script>
